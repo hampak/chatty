d2fe6e96f8453fcb71c994222f2ae61c78239dc2
Haemin Park <haemin.parque@gmail.com>
2024-08-16 21:16:00 +0900

    basic server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("testing build")
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+
+dotenv.config()
+const PORT = process.env.PORT ? process.env.PORT : 8000
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
+app.use(cookieParser())
+app.use(morgan("common"))
+
+app.listen(PORT, () => {
+  console.log(`Server running on port - ${PORT}`)
+})
