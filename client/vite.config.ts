752ebc367257e30d54d828534efb09a21672384b
Haemin Park <haemin.parque@gmail.com>
2024-08-23 16:52:56 +0900

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,16 +4,16 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:8000",
-        target: "https://chatty-server-production-1542.up.railway.app/",
-        changeOrigin: true,
-        secure: true,
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       // target: "http://localhost:8000",
+  //       target: "https://chatty-server-production-1542.up.railway.app/",
+  //       changeOrigin: true,
+  //       secure: true,
+  //     }
+  //   }
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
