4e6fed43c64d8c64991f7431658598ce34cbb12b
Haemin Park <haemin.parque@gmail.com>
2024-08-23 16:22:42 +0900

    Update index.ts

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,11 +1,14 @@
 import axios from "axios"
 import { redirect } from "react-router-dom"
 
+const serverURL = import.meta.env.VITE_API_URL
+
 const checkAuthStatus = async () => {
   try {
-    const response = await axios.get("/api/auth/check-auth", {
-      withCredentials: true
-    })
+    const response = await axios.get(serverURL ? `${serverURL}  "/api/auth/check-auth` : "/api/auth/check-auth"
+      , {
+        withCredentials: true
+      })
 
     const { status } = response
 
