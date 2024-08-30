b96648286795b8eeff6aff393e94044f9f8cbfcf
Haemin Park <haemin.parque@gmail.com>
2024-08-31 01:52:36 +0900

    [IP] - working on the feature of changing online status

--- client/src/components/dashboard/UserSettingsModal.tsx
+++ client/src/components/dashboard/UserSettingsModal.tsx
@@ -23,6 +23,7 @@ const UserSettingsModal = ({ children }: { children: React.ReactNode }) => {
   const { currentStatus } = useSocket()
   const [isCopied, setIsCopied] = useState(false)
   const [selectedStatus, setSelectedStatus] = useState<"online" | "away" | undefined>(undefined)
+  const [open, setOpen] = useState(false)
 
   const form = useForm({
     defaultValues: {
@@ -30,9 +31,19 @@ const UserSettingsModal = ({ children }: { children: React.ReactNode }) => {
     }
   })
 
+  const { isDirty } = form.formState
+  const selectedStatusInForm = form.watch("status")
+
   useEffect(() => {
     setSelectedStatus(currentStatus)
   }, [currentStatus])
+
+  useEffect(() => {
+    if (!open) {
+      form.reset()
+      setSelectedStatus(currentStatus)
+    }
+  }, [open, setOpen, form, currentStatus])
 
   const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text)
@@ -49,8 +60,12 @@ const UserSettingsModal = ({ children }: { children: React.ReactNode }) => {
       })
   }
 
+  const onSubmit = async (values: { status: "online" | "away" | undefined }) => {
+    alert(values.status)
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger className="focus:outline-none">{children}</DialogTrigger>
       <DialogContent
         onInteractOutside={(e) => e.preventDefault()}
@@ -60,7 +75,9 @@ const UserSettingsModal = ({ children }: { children: React.ReactNode }) => {
         <div className="space-y-2">
           <h1 className="text-gray-700 mb-3">Online Status</h1>
           <Form {...form}>
-            <form>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
               <FormField
                 control={form.control}
                 name="status"
@@ -75,58 +92,61 @@ const UserSettingsModal = ({ children }: { children: React.ReactNode }) => {
                           setSelectedStatus(value)
                         }}
                       >
-                        <label className={cn("flex items-center hover:bg-green-100 px-2 py-4 rounded-lg transition-all", selectedStatus === "online" && "bg-green-100")} htmlFor="online">
+                        <label className={cn("flex items-center hover:bg-green-100 px-3 py-4 rounded-lg transition-all", selectedStatus === "online" && "bg-green-100")} htmlFor="online">
                           <div className="space-x-2 flex items-center">
                             <RadioGroupItem value="online" id="online" className="h-3 w-3 bg-green-300 text-green-600 border-green-800" />
                             <Label>Online</Label>
                           </div>
-                          <p className="text-xs text-gray-600 ml-auto">Friends will know you're online</p>
+                          <p className="text-xs text-gray-600 ml-auto">Friends will know you're online :D</p>
                         </label>
-                        <label className={cn("flex items-center hover:bg-orange-100 px-2 py-4 rounded-lg transition-all", selectedStatus === "away" && "bg-orange-100")} htmlFor="away">
+                        <label className={cn("flex items-center hover:bg-orange-100 px-3 py-4 rounded-lg transition-all", selectedStatus === "away" && "bg-orange-100")} htmlFor="away">
                           <div className="space-x-2 flex items-center">
                             <RadioGroupItem value="away" id="away" className="h-3 w-3 bg-orange-300 text-orange-600 border-orange-800" />
                             <Label>Away</Label>
                           </div>
-                          <p className="text-xs text-gray-600 ml-auto">If you don't want to be bothered :D</p>
+                          <p className="text-xs text-gray-600 ml-auto">If you don't want to be bothered :/</p>
                         </label>
                       </RadioGroup>
                     </FormControl>
                   </FormItem>
                 )}
               />
+              <div>
+                <h1 className="text-gray-700 mb-3">User Info</h1>
+                <div className="flex items-center">
+                  <span className="mr-2 text-sm text-gray-800">Your user tag <span className="underline underline-offset-2">{user?.userTag}</span></span>
+                  {
+                    isCopied ? (
+                      <IoCheckmarkOutline
+                        className="text-black"
+                      />
+                    ) : (
+                      <IoCopyOutline
+                        className="hover:cursor-pointer text-gray-500 hover:text-black"
+                        onClick={() => copyToClipboard(user?.userTag || "")}
+                      />
+                    )
+                  }
+                </div>
+              </div>
+              <div className="flex w-full space-x-2 justify-end">
+                <Button
+                  variant={"outline"}
+                  onClick={() => setOpen(false)}
+                >
+                  Close
+                </Button>
+                <Button
+                  type="submit"
+                  disabled={(!isDirty || selectedStatusInForm === currentStatus)}
+                >
+                  Save
+                </Button>
+              </div>
             </form>
           </Form>
         </div>
-        <div>
-          <h1 className="text-gray-700 mb-3">User Info</h1>
-          <div className="flex items-center">
-            <span className="mr-2 text-sm text-gray-800">Your user tag <span className="underline underline-offset-2">{user?.userTag}</span></span>
-            {
-              isCopied ? (
-                <IoCheckmarkOutline
-                  className="text-black"
-                />
-              ) : (
-                <IoCopyOutline
-                  className="hover:cursor-pointer text-gray-500 hover:text-black"
-                  onClick={() => copyToClipboard(user?.userTag || "")}
-                />
-              )
-            }
-          </div>
-        </div>
-        <div className="flex w-full space-x-2 justify-end">
-          <Button
-            variant={"outline"}
-          >
-            Close
-          </Button>
-          <Button
-            type="submit"
-          >
-            Save
-          </Button>
-        </div>
+
       </DialogContent>
     </Dialog>
   )
