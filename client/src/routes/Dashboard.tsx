2981b25902315bda69769b62673d75d9098cddef
Haemin Park <haemin.parque@gmail.com>
2024-09-01 17:55:48 +0900

    Update Dashboard.tsx

--- client/src/routes/Dashboard.tsx
+++ client/src/routes/Dashboard.tsx
@@ -4,11 +4,15 @@ const Dashboard = () => {
 
   const { user } = useUser()
 
+  if (!user) {
+    return null
+  }
+
   return (
     <div className="bg-blue-100s h-full w-full flex items-center justify-center">
       <span className="text-lg font-semibold">
         Hello :) Create or continue with a chat!
-        {user ? user.id : "Not fetched yet"}
+        {user.id}
       </span>
     </div>
   )
