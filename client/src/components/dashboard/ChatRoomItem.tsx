8a67c1a22a54f7cfd4f1cb2bd94f5f0bbe5f44b6
Haemin Park <haemin.parque@gmail.com>
2024-09-08 23:24:52 +0900

    setting this aside for now

--- client/src/components/dashboard/ChatRoomItem.tsx
+++ client/src/components/dashboard/ChatRoomItem.tsx
@@ -37,18 +37,18 @@ const ChatRoomItem = ({ data, friendStatuses, user }: ChatRoomItem) => {
     <div className="absolute bottom-0 right-0 w-3 h-3 bg-gray-500 border-2 border-white rounded-full" />
   );
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    socket.on("message", async (message, senderId, timestamp, chatroomId) => {
-      if (data.id === chatroomId) {
-        setLastMesage(message)
-      }
-    })
+  //   socket.on("message", async (message, senderId, timestamp, chatroomId) => {
+  //     if (data.id === chatroomId) {
+  //       setLastMesage(message)
+  //     }
+  //   })
 
-    return () => {
-      socket.off("message")
-    }
-  }, [data.id, lastMessage])
+  //   return () => {
+  //     socket.off("message")
+  //   }
+  // }, [data.id, lastMessage])
 
   const content = (
     <div className={cn("w-full p-2 rounded-lg hover:bg-gray-100 flex items-center transition-colors", chatId === id ? "cursor-default bg-gray-100" : "hover:cursor-pointer")}>
