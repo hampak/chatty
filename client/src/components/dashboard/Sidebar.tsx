04375a52b72a91aaecbe79b7567bc204140a9bf8
Haemin Park <haemin.parque@gmail.com>
2024-09-12 18:10:10 +0900

    [C] - added some slight UI changes to the sidebar component

--- client/src/components/dashboard/Sidebar.tsx
+++ client/src/components/dashboard/Sidebar.tsx
@@ -40,7 +40,7 @@ const Sidebar = () => {
   );
 
   return (
-    <div className={cn("w-[400px] h-full bg-green-200s py-2 border-r-[1px] bg-white", isOpen ? "flex " : "hidden")}>
+    <div className={cn("w-[400px] h-full bg-green-200s py-2 border-r-[1px] bg-white inline-flex", isOpen ? "fixed left-0 md:relative" : "hidden")}>
 
       {/* Friends List */}
       <FriendsList userId={user?.id} />
